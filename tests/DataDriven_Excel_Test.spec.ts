f2e8aba239b858dfc45f10c2c50c7a0eac4bddfd
jalapathiqa <jalapathiqa@gmail.com>
2026-04-24 21:17:39 -0700

    first commit to branch

---
+++ tests/DataDriven_Excel_Test.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect, chromium } from '@playwright/test';
+import * as XLSX from 'xlsx';
+import path from 'path';
+
+const testDataFilePath = path.join(__dirname, '../TestData/TestData.xlsx');
+
+
+test('Data driven test', async () => {
+
+   const workbook = XLSX.readFile(testDataFilePath);
+   // const sheetName = workbook.SheetNames[0];
+   const worksheet = workbook.Sheets['Sheet1'];
+   interface UserData {
+      email: string;
+      password: string;
+   }
+
+   const data = XLSX.utils.sheet_to_json(worksheet) as UserData[];
+
+   // console.log("TestData: ", data);
+
+   const browser = await chromium.launch({ headless: false });
+   const context = await browser.newContext();
+   const page = await context.newPage();
+   await page.goto('https://bondaracademy.com/login');
+   await page.getByLabel('Email').fill(`${data[2].email}`);
+   await page.getByLabel('Password').fill(`${data[2].password}`);
+   await page.getByText('Login', { exact: true }).click();
+   const errormsg = page.getByText('Invalid email or password')
+   await expect(errormsg).toContainText('Invalid email or password');
+
+
+});
+
+
